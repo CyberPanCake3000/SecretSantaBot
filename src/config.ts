665ab1514b24f1eebf77d6f8007be8c8413b33e3
egatapova <ergatapovaer@gmail.com>
2024-11-19 17:46:50 +0400

    added env variables

--- src/config.ts
+++ src/config.ts
@@ -4,11 +4,15 @@ dotenv.config();
 interface Config {
   telegramBotToken: string;
   mongodbConnString: string;
+  telegramApiAppId: string;
+  telegramApiAppHash: string;
 }
 
 const config: Config = {
   telegramBotToken: process.env.TELEGRAM_BOT_TOKEN || '',
   mongodbConnString: process.env.MONGODB_CONN_STRING || '',
+  telegramApiAppId: process.env.TELEGRAM_API_APP_ID || '',
+  telegramApiAppHash: process.env.TELEGRAM_API_APP_HASH || '',
 };
 
 export default config;
